f865ec43d68ffffe260b2678158a5434263f86c2
Muhammad Baghiz A <ahmadvallden86@gmail.com>
2026-08-23 19:02:08 +0700

    Add configuration for Symphony Course Manager
    Added configuration details and API_URL for the frontend.

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
-
+/**
+ * config.js
+ * Konfigurasi frontend Symphony Course Manager.
+ *
+ * PENTING: Ganti nilai API_URL di bawah dengan URL Web App Google Apps Script Anda
+ * SETELAH melakukan deployment (Deploy > New deployment > Web app).
+ * Contoh: "https://script.google.com/macros/s/AKfycbx....../exec"
+ *
+ * Jangan pernah memasukkan URL asli ke dalam repository publik jika bersifat rahasia.
+ */
+const CONFIG = {
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbzw8NleBNb89pIXCfvWxxsJFyXmsMc13G1s2NH5uUcU88KU3dITtByUbeR8qsgJfE7G/exec",
+  APP_NAME: "Symphony Course Manager",
+  CURRENCY: "IDR",
+};
